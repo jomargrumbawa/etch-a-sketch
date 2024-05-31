8c70dad2c42be2606bcb308ec44026ad8fe02c43
Jomar Rumbawa <jomargrumbawa@gmail.com>
2024-05-31 15:15:13 -0700

    Add reset button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 const sketchpad = document.getElementById("sketchpad")
+const resetBtn = document.getElementById("reset-btn")
+
+resetBtn.addEventListener("click", function() {
+  let newSize = Number(prompt("Enter a number between 16-100"))
+    generateSquares(newSize)
+})
 
 function generateSquares(squares) {
+  sketchpad.innerHTML = ""
   for(let i = 0; i < squares * squares; i++) {
     const square = document.createElement("div")
-    square.style.height = `${600 / 16}px`
-    square.style.width = `${600 / 16}px`
+    square.style.height = `${600 / squares}px`
+    square.style.width = `${600 / squares}px`
     square.addEventListener("mouseenter", function(e) {
       square.classList.add("hovered")
     })
